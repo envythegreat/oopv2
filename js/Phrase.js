d25a85d8b03d69706a2962f4fb7937f1d801b20b
envythegreat <47114375+envythegreat@users.noreply.github.com>
2019-12-16 12:51:44 +0000

    Update Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -2,41 +2,49 @@
  * Project 4 - OOP Game App
  * Phrase.js */
 
+ /**
+  * @class Phrase
+  */
  class Phrase{
+     /**
+      * Constructor
+      * @param {Phrase} phrase 
+      */
      constructor(phrase) {
          this.phrase = phrase.toLowerCase();
      }
-     addPhraseToDisplay(){
-        const phrasePlace =  $('#phrase ul');
-        const selectedPhrase = this.phrase.split('');
-        console.log(this.phrase)
-        //.replace(/\s/g, '')
-        selectedPhrase.forEach(element => {
-            const newLi = $("<li>")
-            newLi.text(`${element}`)
-            phrasePlace.append(newLi)
-            if (element === " ") {
-                newLi.addClass('space')
-            } else {
-                newLi.addClass('hide letter '+ `${element}`+'')
-            }
-            // console.log(phrasePlace);
-        });
-    }
-     checkLetter(letter){
-        let splitPhrase = this.phrase.split('');
-        const checkedLetter = splitPhrase.includes(letter)
-        return checkedLetter;
-     }
-     showMatchedLetter(letter) {
-         const allLetters = $('.letter')
-         allLetters.each((i, element)=>{
-            if (letter === $(element).text()){
-                $(element).removeClass('hide')
-                          .addClass('show')
-            }
-            // console.log($(element).text())
-         })
 
+     /**
+      *  Display phrase on game board 
+      */
+     addPhraseToDisplay(){
+        const phrasePlacehoder =  $('#phrase ul')
+        const letters = this.phrase.split('')
+        
+        letters.forEach(letter => {
+            $('<li>')
+                .text(letter)
+                .addClass(letter === ' ' ? 'space' : `hide letter ${letter}`)
+                .appendTo(phrasePlacehoder)
+        })
+    }
+
+    /**
+     *  Checks to see if the letter selected 
+     * @param {char} letter 
+     * @returns Boolean
+     */
+     checkLetter(letter){   
+        return this.phrase.indexOf(letter) >= 0
+     }
+
+     /**
+      * Reveals the letter(s) on the board that matches the  player's selection
+      * @param {char} letter 
+      */
+     showMatchedLetter(letter) {
+         $(`.letter.${letter}`)
+         .removeClass('hide')
+         .addClass('show')
      }
 }
